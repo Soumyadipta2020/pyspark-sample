5a854649974325e86290e0403bc819a04aad07b0
Soumyadipta Das <63961534+Soumyadipta2020@users.noreply.github.com>
2024-09-30 00:56:32 +0530

    read csv from url

--- sample-code.py
+++ sample-code.py
@@ -1,10 +1,11 @@
 # Create new container - "docker run -it --name pyspark-test apache/spark-py /opt/spark/bin/pyspark"
 # Reopen the older container - "docker start -ai pyspark-test"
 
-from pyspark.sql import SparkSession
+# Spark Session (Already exist in docker container)
+# from pyspark.sql import SparkSession
+# spark = SparkSession.builder.appName("demo").getOrCreate()
 
-spark = SparkSession.builder.appName("demo").getOrCreate()
-
+# Create DataFrame
 df = spark.createDataFrame(
     [
         ("sue", 32),
@@ -16,3 +17,12 @@ df = spark.createDataFrame(
 )
 
 df.show()
+
+# Read csv from web
+import string
+from pyspark import SparkFiles
+data_file_https_url = "https://gist.githubusercontent.com/aakashjainiitg/dbb668c58839d68d7903f508bf55043c/raw/1feec07802b4f53aceac450fa1aee5a87d9276e0/cities_data_bank.csv"
+sc.addFile(data_file_https_url)
+filePath  = 'file://' +SparkFiles.get('cities_data_bank.csv')
+citiesDf = spark.read.csv(filePath, header=True, inferSchema= True)
+citiesDf.show()
