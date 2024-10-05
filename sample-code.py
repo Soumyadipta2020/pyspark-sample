525708a087ec149d963192001800909c29e6a887
Soumyadipta Das <63961534+Soumyadipta2020@users.noreply.github.com>
2024-10-05 23:57:06 +0530

    replace patterns, dimension, Rename, Select columns, Exploratory Data Analysis added

--- sample-code.py
+++ sample-code.py
@@ -12,13 +12,15 @@
 # Spark Session (Already exist in docker container)
 # from pyspark.sql import SparkSession
 # spark = SparkSession.builder.appName("demo").getOrCreate()
+# spark.conf.set("spark.executor.instances", 2)
+# spark.conf.set("spark.executor.cores", 2)
 
 # ============================================================================ #
 # Library
 # ============================================================================ #
 
 from pyspark.sql.functions import col, to_date, year, lower, lit, substring, \
-    weekofyear, date_add, next_day, round, when
+    weekofyear, date_add, next_day, round, when, regexp_replace, avg, max, upper
 
 # ============================================================================ #
 # Create spark DataFrame
@@ -32,7 +34,7 @@ df_sample = spark.createDataFrame(
         ("heo", None),
     ],
     ["first_name", "age"],
-)
+).repartition(2)
 
 df_sample.show()
 
@@ -45,7 +47,7 @@ from pyspark import SparkFiles
 url_df = "https://raw.githubusercontent.com/plotly/datasets/refs/heads/master/all_stocks_5yr.csv"
 sc.addFile(url_df)
 filePath  = 'file://' + SparkFiles.get('all_stocks_5yr.csv')
-all_stocks_5yr = spark.read.csv(filePath, header=True, inferSchema= True)
+all_stocks_5yr = spark.read.csv(filePath, header=True, inferSchema= True).repartition(10)
 all_stocks_5yr.show(3)
 
 # ============================================================================ #
@@ -55,7 +57,7 @@ all_stocks_5yr.show(3)
 data_file_https_url = "https://gist.githubusercontent.com/aakashjainiitg/dbb668c58839d68d7903f508bf55043c/raw/1feec07802b4f53aceac450fa1aee5a87d9276e0/cities_data_bank.csv"
 sc.addFile(data_file_https_url)
 filePath_1  = 'file://' + SparkFiles.get('cities_data_bank.csv')
-citiesDf = spark.read.csv(filePath_1, header=True, inferSchema= True)
+citiesDf = spark.read.csv(filePath_1, header=True, inferSchema= True).repartition(10)
 citiesDf.show(3)
 
 # ============================================================================ #
@@ -63,7 +65,7 @@ citiesDf.show(3)
 # ============================================================================ #
 
 csv_file_path = '/app/data/pyspark/pyspark-sample/nim_output_final.csv'
-df = spark.read.csv(csv_file_path, header=True, inferSchema=True)
+df = spark.read.csv(csv_file_path, header=True, inferSchema=True).repartition(10)
 df.show(3)
 
 # ============================================================================ #
@@ -77,7 +79,7 @@ df_temp.show(5)
 ## Filter data (Date filter)
 all_stocks_5yr_temp = all_stocks_5yr.withColumn("date", to_date("date", "yyyy-MM-dd"))
 ### to check the column types
-all_stocks_5yr_temp.schema 
+all_stocks_5yr_temp.printSchema() 
 
 ### Filter year >= 2016
 all_stocks_5yr_temp = all_stocks_5yr_temp.filter(year(col('date')) >= 2016) 
@@ -133,7 +135,7 @@ df_1 = spark.createDataFrame(
         ("heo", None),
     ],
     ["first_name", "age"],
-)
+).repartition(2)
 
 df_2 = spark.createDataFrame(
     [
@@ -143,7 +145,7 @@ df_2 = spark.createDataFrame(
         ("heon", None),
     ],
     ["first_name", "age"],
-)
+).repartition(2)
 
 unique_values = df_2.select("first_name").distinct().rdd.flatMap(lambda x: x).collect()
 df_temp = df_1.filter(col("first_name").isin(unique_values))
@@ -159,11 +161,83 @@ df_temp = df_sample.withColumn("city", lit("Kolkata")).\
     withColumn("name_prefix", substring("first_name", 1, 2))
 df_temp.show(5)
 
-# Week start from date column, column operation, if else operation
+# Week start from date column, column operation, if else operation, replace a pattern, 
+# uppercase & lowercase
 df_temp = all_stocks_5yr.\
     withColumn("week_start", date_add(next_day(col("date"), "monday"), -7)).\
     withColumn("range", round(col("high") - col("low"), 2)).\
     withColumn("range_ind", when(col("range") > 1, "High").\
-               when((col("range") <= 1) & (col("range") >= 0.5), "Medium").otherwise("Low"))
+               when((col("range") <= 1) & (col("range") >= 0.5), "Medium").otherwise("Low")).\
+    withColumn("week_start_new", regexp_replace(col("week_start"), "-", "/")).\
+    withColumn("range_ind", upper(col("range_ind"))).\
+    withColumn("Name", lower(col("Name")))
 df_temp.show(5)
 
+# ============================================================================ #
+# Replace pattern in column names
+# ============================================================================ #
+old_columns = df_temp.columns
+new_columns = [c.replace('_', ' ') for c in old_columns]
+
+# Use select() to rename the columns
+df_temp = df_temp.select(*[col(c).alias(new_c) for c, new_c in zip(old_columns, new_columns)])
+df_temp.show(5)
+
+# ============================================================================ #
+# Number of rows & columns
+# ============================================================================ #
+
+# Get the number of rows
+num_rows = df_temp.count()
+
+# Get the number of columns
+num_cols = len(df_temp.columns)
+
+# Print the results
+print("Number of rows:", num_rows)
+print("Number of columns:", num_cols)
+
+# ============================================================================ #
+# Rename columns
+# ============================================================================ #
+df_temp = df_temp.\
+    withColumnRenamed("week start new", "week start excel").\
+    withColumnRenamed("range ind", "Variance level").\
+    withColumnRenamed("range", "Variance")
+df_temp.show(5)
+
+# ============================================================================ #
+# Select columns 
+# ============================================================================ #
+
+# Without renaming
+df_temp_new = df_temp.select(
+    "date", "variance"
+)
+df_temp_new.show(5)
+
+# With renaming
+df_temp_new = df_temp.select(
+    col("date").alias("Date"),
+    col("variance").alias("Var")
+)
+df_temp_new.show(5)
+
+# ============================================================================ #
+# Exploratory Data Analysis
+# ============================================================================ #
+
+# Summary Statistics
+df_temp.describe().show()
+
+# Summary Statistics by other variable
+df_temp.groupBy("week start").\
+    agg(
+        round(avg("open"), 2).alias("Average open"),
+        round(avg("high"), 2).alias("Average high"),
+        round(avg("low"), 2).alias("Average low"),
+        round(avg("close"), 2).alias("Average close"),
+        round(avg("volume"), 2).alias("Average volume"),
+        round(max("Variance"), 2).alias("Max Variance")
+    ).\
+    show(5)
